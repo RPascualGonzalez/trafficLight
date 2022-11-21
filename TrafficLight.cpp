f9748a3c1e1bccb62957e1d815cd04dbb73c9a74
Ramon Pascual <Rpascual>
2022-11-21 16:49:34 +0100

    make the count with threads we are not arriving to the end

--- TrafficLight.cpp
+++ TrafficLight.cpp
@@ -2,22 +2,27 @@
 
 #include <iostream>
 #include <thread>
+#include <windows.h>
 
 using namespace std;
 
 
-void trafficLight(string name);
+void trafficLightCount (string name);
+void AskValuesTrafficLight ();
+void StartTrafficLight ();
+void ControlTrafficLightFlags (string name);
+void UpdateTrafficLight (string name);
+int CalculateThreadIdentifier (string name);
 
 int finalValue;
 int initialValue;
 int stepValue;
 int numSteps;
 int actualValue;
+bool TraffickLightFlags[3] = { false, true, false };
 
-void AskValuesTrafficLight();
-void StartTrafficLight();
 
-bool TraffickLightFlags[3] = { true, false, false };
+
 
 int main()
 {
@@ -29,35 +34,85 @@ int main()
 void AskValuesTrafficLight()
 // ----------------------------------------
 {
-	finalValue = 5000;
+	finalValue = 5020;
 	initialValue = 0;
 	stepValue = 20;
 	numSteps = 10;
-	actualValue = 0;
+	actualValue = initialValue;
 }
 
 // ----------------------------------------
 void StartTrafficLight()
 // ----------------------------------------
 {
-	thread threadGreen(trafficLight, "Green");
-	thread threadRed(trafficLight, "Red");
-	thread threadOrange(trafficLight, "Orange");
-
+	thread threadGreen(trafficLightCount, "Green");
+	thread threadRed(trafficLightCount, "Red");
+	thread threadOrange(trafficLightCount, "Orange");
+	
 	threadGreen.join();
 	threadRed.join();
 	threadOrange.join();
+	
 }
 
 // ----------------------------------------
-void trafficLight(string name)
+void trafficLightCount(string name)
 // ----------------------------------------
 {
-	for (int value = initialValue; value < 1000; value = value + stepValue)
+	while (actualValue < finalValue)
 	{
-		cout << name << "\n";
+		ControlTrafficLightFlags(name);
+		int stepFinalValue = actualValue + (numSteps * stepValue);
+		if (stepFinalValue > finalValue)
+		{
+			stepFinalValue = finalValue;
+		}
+		for (; actualValue < stepFinalValue; actualValue = actualValue + stepValue)
+		{
+			cout << actualValue << " name: " << name << "\n";
+		}
+		cout << "-----change----- \n";
+		UpdateTrafficLight(name);
 	}
-
+	
 }
 
+// ----------------------------------------
+void ControlTrafficLightFlags(string name)
+// ----------------------------------------
+{
+	int threadIdentifier = CalculateThreadIdentifier(name);
+
+	while (not TraffickLightFlags [threadIdentifier])
+	{
+		Sleep(10);
+	}
+}
+
+// ----------------------------------------
+void UpdateTrafficLight(string name)
+// ----------------------------------------
+{
+	int threadIdentifier = CalculateThreadIdentifier(name);
+	TraffickLightFlags[threadIdentifier] = false;
+	if (threadIdentifier == 2)
+	{
+		TraffickLightFlags[0] = true;
+	}
+	else {
+		TraffickLightFlags[threadIdentifier + 1] = true;
+	}
+}
+
+// ----------------------------------------
+int CalculateThreadIdentifier(string name)
+// ----------------------------------------
+{
+	int result = 0;
+	if (name == "Green") result = 0;
+	else if (name == "Red") result = 1;
+	else if (name == "Orange") result = 2;
+
+	return result;
+}
 
